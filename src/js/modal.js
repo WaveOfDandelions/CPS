51db205b56bd9cd82bf2e1cb15208471298d8194
Wave <www.dandelion.ru@gmail.com>
2024-06-10 17:03:16 +0300

    Add second modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,7 @@
 let callButton = document.querySelectorAll(".button-call");
-let modalCall = document.querySelector(".modal__wrapper");
-let modalClose = document.querySelector(".modal__close");
+let feedbackButton = document.querySelectorAll(".button-feedback");
+let modalCall = document.querySelectorAll(".modal__wrapper");
+let modalClose = document.querySelectorAll(".modal__close");
 let overlayContent = document.querySelector(".overlay__content");
 
 let appWrapper = document.querySelector(".app-wrapper");
@@ -16,19 +17,35 @@ for (let i = 0; i < callButton.length; i++) {
 		header.classList.toggle("faded");
 		overlayContent.classList.toggle("faded");
 		appWrapper.classList.toggle("faded");
-		modalCall.classList.toggle("open");
-		modalClose.classList.add("open");
+		modalCall[0].classList.toggle("open");
+		modalClose[0].classList.add("open");
 	});
 }
 
-modalClose.addEventListener("click", (e) => {
-	e.preventDefault();
+for (let i = 0; i < feedbackButton.length; i++) {
+	feedbackButton[i].addEventListener("click", (e) => {
+		e.preventDefault();
 
-	body.classList.toggle("restrictScroll");
-	body.classList.add("restrictScroll");
-	modalCall.classList.remove("open");
-	header.classList.toggle("faded");
-	overlayContent.classList.toggle("faded");
-	appWrapper.classList.toggle("faded");
-	modalClose.classList.remove("open");
-});
+		body.classList.toggle("restrictScroll");
+		body.classList.add("restrictScroll");
+		header.classList.toggle("faded");
+		overlayContent.classList.toggle("faded");
+		appWrapper.classList.toggle("faded");
+		modalCall[1].classList.toggle("open");
+		modalClose[1].classList.add("open");
+	});
+}
+
+for (let i = 0; i < modalClose.length; i++) {
+	modalClose[i].addEventListener("click", (e) => {
+		e.preventDefault();
+
+		body.classList.toggle("restrictScroll");
+		body.classList.add("restrictScroll");
+		modalCall[i].classList.remove("open");
+		header.classList.toggle("faded");
+		overlayContent.classList.toggle("faded");
+		appWrapper.classList.toggle("faded");
+		modalClose[i].classList.remove("open");
+	});
+}
